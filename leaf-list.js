63cf433dcb3501cb4eba6ebbd5cd2978da5c816b
Hilal38 <hhilalkocak@gmail.com>
2024-04-03 09:19:20 +0300

    Add recursive

--- leaf-list.js
+++ leaf-list.js
@@ -1,3 +1,6 @@
+// Write a function, leafList, that takes in the root of a binary tree and returns an array containing the values of all leaf nodes in left-to-right order.
+
+
 class Node {
   constructor(val) {
     this.val = val;
